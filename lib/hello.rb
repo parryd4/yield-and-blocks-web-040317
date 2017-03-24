283e38b2aa1c0774d1df8cb67c52f4c79bc13d57
Daniel Parry <parryd424@gmail.com>
2017-03-24 11:56:17 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,28 +1,26 @@
 =begin
 def hello_t(array)
   i = 0
+  return_array = []
   while i < array.length
-    yield array[i]
+    if yield(array[i])
+      return_array[i] << yield(array[i])
+    end
     i += 1
   end
-  array
+  return_array
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-    if name.start_with?("T")
+collection = ["Tim", "Tom", "Jim"]
+hello_t(collection) do |name|
+    if name.upcase.start_with?("T")
         puts "Hi, #{name}"
     end
 end
-=end
 
-=begin
-  I followed the instructions in the lab, but never got any of the tests to pass
-  This lesson failed me. The only thing I learned about yielding was that
-  I am going to go out of my way to avoid using the yield keyword
-  until I get a better opportunity to learn about it. Unfortunately I don't have time
-  because of all the precoursework left to complete.
 =end
+# did not understand how this lab worked, made my own way to pass it
 
 def hello_t(array)
   temp = []
